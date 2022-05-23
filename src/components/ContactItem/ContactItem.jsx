5aae1953ad6f7afbf0b909ba12e0330f03d36e69
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2022-05-23 19:04:34 +0300

    +
    +

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -64,7 +64,7 @@ export const ContactItem = ({ contact }) => {
         onClick={onEnter}
         onFocus={onEnter}
         onBlur={onBlur}
-        tabIndex={0}
+        tabIndex={1}
       >
         <TextBox>
           <span>{name}</span>
